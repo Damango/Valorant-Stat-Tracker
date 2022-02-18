e7113cd7f5a4955d4d0fda32f1ffe250457ff074
Justin Kessler <redcoderhd1@gmail.com>
2022-02-18 13:07:04 -0500

    First commit

---
+++ src/ValorantTracker.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ValorantTracker.css";
+
+const ValorantTracker = (props) => {
+	return <div className="valorant-tracker-container"></div>;
+};
+
+export default ValorantTracker;
