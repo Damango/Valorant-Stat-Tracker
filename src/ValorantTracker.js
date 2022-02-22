465a46fc4dafe75915fb41be884e0b4f33f42cbb
Justin Kessler <redcoderhd1@gmail.com>
2022-02-21 22:32:52 -0500

    Data Structure Setup

--- src/ValorantTracker.js
+++ src/ValorantTracker.js
@@ -1,43 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ValorantTracker.css";
-import MatchCard from "./Components/MatchCard/MatchCard"
+import MatchCard from "./Components/MatchCard/MatchCard";
 
+import MatchData from "./Match Data/MatchData";
 const ValorantTracker = (props) => {
+	const [currentPlayer, setCurrentPlayer] = useState("Busters#zyzz");
 
+	return (
+		<div className="valorant-tracker-container">
+			<div className="nav-bar-container"></div>
 
-
-	let matchHistory = [1, 2, 3, 4, 5]
-
-
-
-	return <div className="valorant-tracker-container">
-		<div className="nav-bar-container"></div>
-
-		<div className="main-view-container">
-			<div className="main-view-wrapper">
-				<div className="profile-summary-container">
-					<div className="account-name-rank-container">BUSTERS#Zyzz</div>
-					<div className="favorite-agent-container"></div>
-
-				</div>
-				<div className="match-history-container">
-					<div className="match-history-container-header">Match History</div>
-					<div className="match-history-wrapper">
-						<div className="match-history-summary">
-
-						</div>
-						<div className="match-history-card-list-container">
-							{matchHistory.map((match, index) => <MatchCard index={index} />)}
+			<div className="main-view-container">
+				<div className="main-view-wrapper">
+					<div className="profile-summary-container">
+						<div className="account-name-rank-container">BUSTERS#Zyzz</div>
+						<div className="favorite-agent-container"></div>
+					</div>
+					<div className="match-history-container">
+						<div className="match-history-container-header">Match History</div>
+						<div className="match-history-wrapper">
+							<div className="match-history-summary"></div>
+							<div className="match-history-card-list-container">
+								{MatchData.map((match, index) => (
+									<MatchCard
+										matchData={match}
+										index={index}
+										currentPlayer={currentPlayer}
+									/>
+								))}
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-
 		</div>
-
-
-
-	</div>;
+	);
 };
 
 export default ValorantTracker;
